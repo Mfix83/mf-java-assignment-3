19287a6eccf4a3b64588ee755cde537532ebe8ea
Mfix83 <123844738+Mfix83@users.noreply.github.com>
2023-08-01 17:22:27 -0400

    Updated Code

--- src/com/coderscampus/User.java
+++ src/com/coderscampus/User.java
@@ -1,32 +1,59 @@
 package com.coderscampus;
 
 public class User {
-	
-	@Override
-	public String toString() {
-		return "User [username=" + username + ", password=" + password + ", name=" + name + "]";
-	}
+    private String username;
+    private String password;
+    private String name;
 
-	private String username;
-	private String password;
-	private String name;
+    public User(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
 
-	public User(String username, String password, String name) {
-		
-	this.username = username;
-    this.password = password;
-    this.name = name;
-		
-	}
-	public String getUsername () {
-		return username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	}
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+//package com.coderscampus;
+//
+//public class User {
+//	
+//	@Override
+//	public String toString() {
+//		return "User [username=" + username + ", password=" + password + ", name=" + name + "]";
+//	}
+//
+//	private String username;
+//	private String password;
+//	private String name;
+//
+//	public User(String username, String password, String name) {
+//		
+//	this.username = username;
+//    this.password = password;
+//    this.name = name;
+//		
+//	}
+//	public String getUsername () {
+//		return username;
+//	}
+//	public String getPassword() {
+//		return password;
+//	}
+//	
+//	public String getName() {
+//		return name;
+//	}
+//	}
 
